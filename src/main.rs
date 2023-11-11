5b6ae1255bf6ceb5675027f160c3d42d00d7cc13
Martin Arce <martin.arce.teixeira@gmail.com>
2023-11-11 21:37:04 +0100

    Chiki

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,27 @@
+#[warn(unused_variables)]
 use person::Person;
 mod person;
 
+use std::fs::File;
+use std::io::prelude::*;
+use std::io::{self, BufReader};
+
 fn main() {
-    let m = String::from("meo");
+    let db = read_db();
+    // println!("{:?}", db);
+}
+
+fn read_db() -> Vec<Person> {
+    let f = File::open("./db.txt").unwrap();
+    let mut reader = BufReader::new(f);
+    let mut line = String::from("LINE");
+    reader.read_line(&mut line).unwrap();
+    println!("IS: {:?}", line);
+    reader.read_line(&mut line).unwrap();
+    println!("IS: {:?}", line);
+
+    let m = String::from("Chiki");
     let p: Person = Person::new(m);
-    println!("IS: {:?}", p.name);
+
+    vec![p]
 }
